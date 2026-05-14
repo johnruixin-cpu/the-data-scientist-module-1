8d10cbc789dd5c3dddfcc0e1eba0f89d707b9dc2
johnruixin-cpu <johnruixin@gmail.com>
2026-05-14 19:56:59 -0400

    Delete data directory

--- data/count_freq.py
+++
@@ -1,6 +0,0 @@
-def count_frequency(entries):
-    counts = {} # set up an empty dictionary to hold counts
-    for entry in entries:
-        category = entry.split(":")[0] # create sublists by splitting on ":" and selcting the first element in each list
-        counts[category] = counts.get(category, 0) + 1 # accumulate counts for each category
-    return counts
